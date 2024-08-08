e849905cc874008cadc8ceb68d2c0dd2f0c7d58d
uvini917 <65717498+uvini917@users.noreply.github.com>
2024-08-08 19:28:06 +0530

    login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,34 @@
-function LoginPage() {
+import { BuiltInProviderType } from 'next-auth/providers/index';
+import { ClientSafeProvider, getProviders, LiteralUnion } from 'next-auth/react'
+
+// interface LoginPageProps{
+//   providers: Record<LiteralUnion<BuiltInProviderType>, ClientSafeProvider> 
+// }
+
+const LoginPage = async () =>  {
+  const providers = await getProviders();
+
   return (
-    <div>Login Page Here</div>
+    <div className="flex items-center justify-center h-screen">
+      {providers ? (
+        Object.values(providers).map((p: ClientSafeProvider) => (
+          <div key={p.id}>Sign in with {p.name}</div>
+        ))
+      ) : (
+        <div>No providers available</div>
+      )}
+    </div>
   )
 }
 
 export default LoginPage
+
+// export async function getServerSideProps() {
+
+//   const providers = await getProviders();
+//   console.log("get server sider pros function is called.");
+//   return {   
+//     props: {providers},
+//   }
+  
+// }
