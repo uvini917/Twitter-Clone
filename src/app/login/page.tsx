9e771997c923b000a67984dda115d89c4f19d465
uvini917 <65717498+uvini917@users.noreply.github.com>
2024-08-13 19:19:59 +0530

    signin button

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,34 +1,43 @@
-import { BuiltInProviderType } from 'next-auth/providers/index';
-import { ClientSafeProvider, getProviders, LiteralUnion } from 'next-auth/react'
+import { BuiltInProviderType } from "next-auth/providers/index";
+import {
+  ClientSafeProvider,
+  getProviders,
+  LiteralUnion,
+} from "next-auth/react";
 
 // interface LoginPageProps{
-//   providers: Record<LiteralUnion<BuiltInProviderType>, ClientSafeProvider> 
+//   providers: Record<LiteralUnion<BuiltInProviderType>, ClientSafeProvider>
 // }
 
-const LoginPage = async () =>  {
+const LoginPage = async () => {
   const providers = await getProviders();
 
   return (
     <div className="flex items-center justify-center h-screen">
       {providers ? (
         Object.values(providers).map((p: ClientSafeProvider) => (
-          <div key={p.id}>Sign in with {p.name}</div>
+          <div key={p.id}>
+            <button className='flex items-center bg-twitterWhite pr-4 py-2 text-black rounded-full'>
+
+              <img src="/google.png" className='h-6 px-3' alt="" />
+              Sign in with {p.name}</button>
+          </div>
         ))
       ) : (
         <div>No providers available</div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
 
 // export async function getServerSideProps() {
 
 //   const providers = await getProviders();
 //   console.log("get server sider pros function is called.");
-//   return {   
+//   return {
 //     props: {providers},
 //   }
-  
+
 // }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        twitterWhite: "#e7e9ea",
+        twitterBlue: "#308cd8",
+        twitterBorder: "#2f3336",
+        twitterLighGray: "#71767b",
+        twitterDarkGray: "#17181c"
+      }
+    },
   },
   plugins: [],
 }
